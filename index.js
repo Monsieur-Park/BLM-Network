9b6c2835040b046a21d5e48b6b668df4044e3a0a
Yohan Park <36798970+Monsieur-Park@users.noreply.github.com>
2020-09-14 19:12:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function scaledSize(d) {
 
 
 
-d3.json("BLM_network_final.json", function(error, graph) {
+d3.json("BLM_Graph.json", function(error, graph) {
   if (error) throw error;
 
   var link = container.append("g")
